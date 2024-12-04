c04cea8f09c6300b33a16543ed26f1c63d8adc2e
joshdei <joshuadeinne@gmail.com>
2024-12-04 10:59:21 +0100

    k

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-            <x-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
-                {{ __('login') }}
-            </x-nav-link>
-        </div>
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-            <x-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-                {{ __('register') }}
-            </x-nav-link>
-        </div>
-    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
