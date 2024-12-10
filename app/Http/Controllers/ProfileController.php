7eb62f4f86c6bff75fbf0e15138270cd799a7e21
joshdei <joshuadeinne@gmail.com>
2024-12-10 10:21:17 +0100

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -17,30 +18,24 @@ class ProfileController extends Controller
         return view('profile', compact('user', 'items', 'postCount'));
     }
    
+    
     public function updateProfilePicture(Request $request)
-    {
-        $request->validate([
-            'profile_picture' => 'image|mimes:jpg,png,jpeg|max:2048',
-        ]);
-    
-        try {
-            if ($request->hasFile('profile_picture')) {
-                $file = $request->file('profile_picture');
-                $fileName = Str::random(10) . '_' . time() . '.' . $file->getClientOriginalExtension();
-                
-                $filePath = $file->storeAs('profile_pictures', $fileName, 'public');
-                $user = Auth::user();
-                if ($user) {
-                    $user->profile_photo_path = $filePath;
-                    $user->save();
-                } else {
-                    return back()->with('error', 'User not authenticated. Please login first.');
-                }
-            }
-            return back()->with('success', 'Profile picture updated successfully.');
-        } catch (\Exception $e) {
-            return back()->with('error', 'There was an error updating your profile picture. Please try again later.');
-        }
+{
+    $request->validate([
+        'profile_picture' => 'image|mimes:jpg,png,jpeg|max:2048',
+    ]);
+
+    if ($request->hasFile('profile_picture')) {
+        $filePath = $request->file('profile_picture')->store('profile_pictures', 'public');
+        $user_id = Auth::id();
+        $user = User::findOrFail($user_id);
+        $user->profile_photo_path = $filePath;
+        $user->save();
+
+        return back()->with('success', 'Profile picture updated successfully.');
     }
-    
+
+    return back()->with('error', 'No profile picture uploaded.');
+}
+
 }
