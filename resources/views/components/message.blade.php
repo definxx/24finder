cd7fea5a0511d7e79080bbfc085855e30a9fe9c6
joshdei <joshuadeinne@gmail.com>
2024-12-10 21:38:44 +0100

    Update message.blade.php

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -30,7 +30,7 @@
                 @if($message->sender_id == Auth::id())
                 <div class="flex-shrink-0 w-10 h-10 rounded-full overflow-hidden">
                     <img 
-                    src="{{ $Auth::user()->profile_photo_path ? asset('storage/app/public/' . $Auth::user()->profile_photo_path) : asset('images/logo.jpg') }}"
+                    src="{{ Auth::user()->profile_photo_path ? asset('storage/app/public/' . Auth::user()->profile_photo_path) : asset('images/logo.jpg') }}"
                    
                      alt="You" class="w-full h-full object-cover" />
                 </div>
