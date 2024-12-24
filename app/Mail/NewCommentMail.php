ee120066e4a8125ff15c72db59a6b1b09f459556
joshdei <joshuadeinne@gmail.com>
2024-12-24 14:22:15 +0100

    Update NewCommentMail.php

--- app/Mail/NewCommentMail.php
+++ app/Mail/NewCommentMail.php
@@ -28,7 +28,8 @@ class NewCommentMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'New Comment ',
+            subject: 'New Comment '.  $this->comment->content
+        
         );
     }
 
