fc854fcecc805fe4ca8285b1e0f6d5233eb0ac9f
joshdei <joshuadeinne@gmail.com>
2024-12-29 09:54:25 +0100

    Update notify-users.blade.php

--- resources/views/emails/notify-users.blade.php
+++ resources/views/emails/notify-users.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>24Finder.ng - Effortless Trading</title>
+    <title>Selling something you don’t need any more on 24Finder.ng</title>
     <style>
         /* General styles */
         body {
